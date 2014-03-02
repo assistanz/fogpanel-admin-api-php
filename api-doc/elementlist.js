1116276e60bb0078511fd5636ded490ec7ea5f82
Sujai SD <sujaisd@gmail.com>
2014-03-03 00:44:58 +0530

    Added API Documentation of FogAPI.

---
+++ api-doc/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","AssistanZ\\FogPanel\\API\\Admin\\Account"],["c","AssistanZ\\FogPanel\\API\\Admin\\Billing"],["c","AssistanZ\\FogPanel\\API\\Admin\\Config"],["c","AssistanZ\\FogPanel\\API\\Admin\\Firewall"],["c","AssistanZ\\FogPanel\\API\\Admin\\ServiceOffering"],["c","AssistanZ\\FogPanel\\API\\Admin\\Template"],["c","AssistanZ\\FogPanel\\API\\Admin\\VirtualMachine"]];
